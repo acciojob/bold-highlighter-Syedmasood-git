bdcb3bffce7de0ed896628653181060fa460dbca
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-22 00:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function highlight() {
-    //Write your code here
-let bold=document.querySelectorAll("p strong");
-	for(let i=0;i<=bold.length;i++){
-		bold[i].style.color="green";
-	}
+    var boldWords = document.querySelectorAll("p strong");
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = "green";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-let boldWords = document.querySelectorAll("p strong");
-    for (var i = 0; i < boldWords.length; i++) {
-        boldWords[i].style.color = "black";
-    }
-    
+    var boldWords = document.querySelectorAll("p strong");
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = "black";
+    });
 }
