ae9467b5bdb10d5c4d3249e334512675b07fc6c1
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-22 00:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+let bold=document.querySelectorAll("p strong");
+	for(let i=0;i<=bold.length;i++){
+		bold[i].style.color="green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+let boldWords = document.querySelectorAll("p strong");
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "black";
+    }
     
 }
